1e30cfc3287b4b13cfb50296c2268c31b0674d36
saadullahkhan51 <saadullahkhan5151@gmail.com>
2022-12-10 02:47:42 +0500

    Post new

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -10,3 +10,7 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name: str = 'post_new.html'
+    fields = ['title', 'body', 'author']
